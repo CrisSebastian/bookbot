4099479051f7d0b017e984bb635b3772f7e6a966
Sebastian <cristofolisebastian@gmail.com>
2025-03-22 09:31:16 +0000

    updated to the new version with sys, had to cheat with the docs, somehow i dont get the same amount of letters

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 from ast import keyword
-
+from stats import word_count
+import sys
 
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book_path = sys.argv[1]
+
     book = get_book_text(book_path)
     num_words = word_count(book)
     chars_dict = char_count(book)
     report = gen_report(chars_dict, book_path, num_words)
-
-def word_count(book):
-    words = book.split()
-    return len(words)
 
 def char_count(book):
     standarized_book = book.lower()
@@ -28,7 +30,7 @@ def sort_on(dict):
 def gen_report(chars_dict, path, amount):
     char_list = []
     print(f"--- Begin report of {path} ---")
-    print(f"{amount} words found in the book")
+    print(f"{amount} words found in the document")
     for char, num in chars_dict.items():
         char_list.append({"char": char, "num": num})
 
@@ -36,7 +38,7 @@ def gen_report(chars_dict, path, amount):
 
     for char_dict in char_list:
         if char_dict["char"].isalpha():
-            print(f"The '{char_dict["char"]}' was found {char_dict["num"]} times")
+            print(f"{char_dict["char"]}: {char_dict["num"]}")
         else:
             pass
     print("--- End of report ---")
